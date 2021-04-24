67b86cfe84750848de177f9c80e5a4a3611607f0
MarcosRodrigues <marcosalexandre73@gmail.com>
2021-04-24 08:44:44 -0300

    envioInformacoes

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
 import express, { request, response } from "express"
 import "./database";
+import { routes} from "./routes"
 
 const app = express();
 
+app.use(express.json());
+
+app.use(routes);
+
 app.listen(1973, () =>  console.log("Server is running on port 1973"));
 
-app.get("/", ( request, response ) =>{
+
+
+/**
+ * app.get("/", ( request, response ) =>{
     return response.json({
         message: "Olá Marcos, que bom que está aqui!",
     });
@@ -16,3 +24,4 @@ app.post("/", (request, response) =>{
         message: "Usuário salvo com sucesso!!",
     });
 });
+ */
