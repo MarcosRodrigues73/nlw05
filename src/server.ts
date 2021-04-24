de1e1515487d122575eb3731207f9222f178a482
MarcosRodrigues <marcosalexandre73@gmail.com>
2021-04-23 22:19:30 -0300

    finaldiaumfunfando100porcento

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,17 @@
-import express from "express"
+import express, { request, response } from "express"
 
 const app = express();
 
 app.listen(1973, () =>  console.log("Server is running on port 1973"));
+
+app.get("/", ( request, response ) =>{
+    return response.json({
+        message: "Olá Marcos, que bom que está aqui!",
+    });
+});
+
+app.post("/", (request, response) =>{
+    return response.json({
+        message: "Usuário salvo com sucesso!!",
+    });
+});
